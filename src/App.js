d18bbcc11f655e38e210dd74df79b71869836f5e
Max <MMCleodWebDev@gmail.com>
2022-04-18 03:43:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import SectionOne from './components/SectionOne';
+import SectionThree from './components/SectionThree';
+import SectionTwo from './components/SectionTwo';
+import Stories from './components/Stories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <Stories />
+      <Features />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='bg-black'>
+            {/* <img className='m-auto pt-10' src="../assets/svg/logo-white.svg" alt="" />
+            <div className='text-white text-center grid md:grid-cols-4 grid-rows-4 grid-flow-col'>
+                <p className=''>social media icons</p>
+                <div className='font-semibold tracking-widest'>
+                    <p className='md:inline md:ml-5 pb-3'>HOME</p>
+                    <p className='md:inline md:ml-5 pb-3'>STORIES</p>
+                    <p className='md:inline md:ml-5 pb-3'>FEATURES</p>
+                    <p className='md:inline md:ml-5'>PRICING</p>
+                </div>
+                <p className='font-semibold tracking-widest mt-20'>GET AN INVITE</p>
+                <p>CopyRight 2019. All Rights Reserved</p>
+            </div> */}
+
+            <div className='grid grid-cols-2'>
+
+            </div>
+            <img className='m-auto pt-10' src="../assets/svg/logo-white.svg" alt="" />
+            <div className='flex flex-col md:flex-col-reverse justify-start text-white text-center'>
+                <p className=''>social media icons</p>
+                <div className='font-semibold tracking-widest'>
+                    <p className='md:inline md:ml-5 pb-3'>HOME</p>
+                    <p className='md:inline md:ml-5 pb-3'>STORIES</p>
+                    <p className='md:inline md:ml-5 pb-3'>FEATURES</p>
+                    <p className='md:inline md:ml-5'>PRICING</p>
+                </div>
+            </div>
+            <p className='font-semibold tracking-widest mt-20'>GET AN INVITE</p>
+            <p>CopyRight 2019. All Rights Reserved</p>
+        </div >
+    )
+}
