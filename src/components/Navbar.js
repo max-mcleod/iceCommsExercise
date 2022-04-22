cd497cd9626f90f9c4dc39e1446bfb1b121eda1f
Max Mcleod <maxwellmcleod@gmail.com>
2022-04-21 17:07:52 -0700

    Removed unnecessary files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
   return (
     <nav className='flex justify-between xl:mx-32 p-5'>
       <img className='object-none' src="https://ice-nas.fra1.digitaloceanspaces.com/development/play/logo.svg" alt="" />
-      <button id="hamburger" className='md:hidden block' >
+      <button className='md:hidden block' >
         <img className='w-14 h-14' src="../assets/menu.png" alt="" />
       </button>
-      <ul id="navUL" className='m-auto md:block hidden md:static absolute'>
+      <ul className='md:static md:block m-auto hidden absolute'>
         <li className='inline font-semibold tracking-widest'><a href="/">STORIES</a></li>
         <li className='inline px-10 font-semibold tracking-widest'><a href="/">FEATURES</a></li>
         <li className='inline font-semibold tracking-widest'><a href="/">PRICING</a></li>
